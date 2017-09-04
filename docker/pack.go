b631f5bdef8dbe52395a03004f79cbd03baffd16
Michael Schenk <michael.schenk@weather.com>
2017-09-04 15:39:28 -0500

    Add start and enable pack actions

--- docker/pack.go
+++ docker/pack.go
@@ -5,6 +5,8 @@ import (
 	"github.com/mschenk42/gopack/action"
 )
 
+var dockerTask Docker
+
 // Run initializes the properties and runs the pack
 func Run(props *gopack.Properties, actions []string) {
 	pack := gopack.Pack{
@@ -17,7 +19,12 @@ func Run(props *gopack.Properties, actions []string) {
 		ActionMap: map[string]func(p *gopack.Pack){
 			"default": run,
 			"install": install,
+			"start":   start,
 		},
+	}
+	dockerTask = Docker{
+		Version:  pack.Props.Str("docker.version"),
+		EdgeRepo: pack.Props.Bool("docker.enable-edge-repo"),
 	}
 	pack.Run(props)
 }
@@ -26,8 +33,9 @@ func run(pack *gopack.Pack) {
 }
 
 func install(pack *gopack.Pack) {
-	Docker{
-		Version:  pack.Props.Str("docker.version"),
-		EdgeRepo: pack.Props.Bool("docker.enable-edge-repo"),
-	}.Run(action.Install)
+	dockerTask.Run(action.Install)
+}
+
+func start(pack *gopack.Pack) {
+	dockerTask.Run(action.Enable, action.Start)
 }
--- docker/task.go
+++ docker/task.go
@@ -6,9 +6,10 @@ import (
 	"github.com/mschenk42/gopack"
 	"github.com/mschenk42/gopack/action"
 	"github.com/mschenk42/gopack/task"
+	"github.com/mschenk42/systemd-runpack/systemd"
 )
 
-// Docker ...
+// Docker installs and configures the Docker service for a host
 type Docker struct {
 	Version  string
 	EdgeRepo bool
@@ -25,68 +26,82 @@ func (d Docker) Run(runActions ...action.Enum) gopack.ActionRunStatus {
 func (d Docker) registerActions() action.Methods {
 	return action.Methods{
 		action.Install: d.install,
+		action.Enable:  d.enable,
+		action.Start:   d.start,
 	}
 }
 
 func (d *Docker) setDefaults() {
-	if d.Version == "" {
-		d.Version = "default value"
-	}
 }
 
 // String returns a string which identifies the task with it's property values
 func (d Docker) String() string {
-	return fmt.Sprintf("docker %s edge: %t", d.Version, d.EdgeRepo)
+	return fmt.Sprintf("docker %s edge %t", d.Version, d.EdgeRepo)
 }
 
 func (d Docker) install() (bool, error) {
-	const dockerBreadcrumb = "/var/run/docker-remove"
+	const dockerBreadcrumb = "/var/run/docker-installed"
 
-	setBreadcrumb := task.Command{
-		Name: "touch",
-		Args: []string{dockerBreadcrumb},
+	_, breadcrumb, err := task.Fexists(dockerBreadcrumb)
+	if err != nil {
+		return false, err
 	}
 
-	removeDocker := task.Command{
-		Name:   "yum",
-		Args:   []string{"remove", "-y", "docker", "docker-common", "docker-selinux", "docker-engine"},
-		Stream: true,
+	if !breadcrumb {
+		task.Command{
+			Name:   "yum",
+			Args:   []string{"remove", "-y", "docker", "docker-common", "docker-selinux", "docker-engine"},
+			Stream: true,
+		}.Run(action.Run)
+
+		task.Command{
+			Name:   "yum",
+			Args:   []string{"install", "-y", "yum-utils", "device-mapper-persistent-data", "lvm2"},
+			Stream: true,
+		}.Run(action.Run)
+
+		task.Command{
+			Name:   "yum-config-manager",
+			Args:   []string{"--add-repo", "https://download.docker.com/linux/centos/docker-ce.repo"},
+			Stream: true,
+		}.Run(action.Run)
+
+		enableEdgeRepo := task.Command{
+			Name:   "yum-config-manager",
+			Args:   []string{"--enable", "docker-ce-edge"},
+			Stream: true,
+		}
+		enableEdgeRepo.SetOnlyIf(func() (bool, error) { return d.EdgeRepo, nil })
+		enableEdgeRepo.Run(action.Run)
+
+		task.Command{
+			Name:   "yum",
+			Args:   []string{"makecache", "fast"},
+			Stream: true,
+		}.Run(action.Run)
+
+		task.Command{
+			Name:   "yum",
+			Args:   []string{"install", "-y", d.Version},
+			Stream: true,
+		}.Run(action.Run)
+
+		task.Command{
+			Name: "touch",
+			Args: []string{dockerBreadcrumb},
+		}.Run(action.Run)
+
+		return true, nil
 	}
-	removeDocker.SetNotIf(func() (bool, error) { _, x, err := task.Fexists(dockerBreadcrumb); return x, err })
-	removeDocker.SetNotify(setBreadcrumb, action.Run, action.Run, true)
-	removeDocker.Run(action.Run)
+	return false, nil
+}
 
-	task.Command{
-		Name:   "yum",
-		Args:   []string{"install", "-y", "yum-utils", "device-mapper-persistent-data", "lvm2"},
-		Stream: true,
-	}.Run(action.Run)
+func (d Docker) start() (bool, error) {
+	status := systemd.SystemCtl{Service: "docker"}.Run(action.Start)
+	return status[action.Start], nil
+}
 
-	task.Command{
-		Name:   "yum-config-manager",
-		Args:   []string{"--add-repo", "https://download.docker.com/linux/centos/docker-ce.repo"},
-		Stream: true,
-	}.Run(action.Run)
-
-	enableEdgeRepo := task.Command{
-		Name:   "yum-config-manager",
-		Args:   []string{"--enable", "docker-ce-edge"},
-		Stream: true,
-	}
-	enableEdgeRepo.SetOnlyIf(func() (bool, error) { return d.EdgeRepo, nil })
-	enableEdgeRepo.Run(action.Run)
-
-	task.Command{
-		Name:   "yum",
-		Args:   []string{"makecache", "fast"},
-		Stream: true,
-	}.Run(action.Run)
-
-	task.Command{
-		Name:   "yum",
-		Args:   []string{"install", "-y", d.Version},
-		Stream: true,
-	}.Run(action.Run)
-
-	return true, nil
+func (d Docker) enable() (bool, error) {
+	status := systemd.SystemCtl{Service: "docker"}.Run(action.Enable)
+	return status[action.Enable], nil
 }
